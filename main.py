544001e16303aa3c4fca9e73c941ddc295bc9967
vincentdeaugustine <vinnied44@csu.fullerton.edu>
2019-10-19 13:30:43 -0700

    Trying to get mario to walk

--- main.py
+++ main.py
@@ -20,9 +20,59 @@ def run_game():
     # Set up a screen and give it the dimensions we want
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption('Super Mario')
+    BACKGROUND_COLOR = pygame.Color('white')
+
+
 
     # Initialize the level we are on
-    level = Level(level_dict[level_counter])
+    # level = Level(level_dict[level_counter])
     while True:
         pass
-run_game()
+
+
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Super Mario')
+BACKGROUND_COLOR = pygame.Color('white')
+
+
+def mario_walk(direction, passed_screen, bgc):
+    images = []
+    if direction == 'left':
+        pass
+    if direction == 'right':
+        images.append(pygame.image.load('Resources/Sprites/Mario/walk_r_1.png'))
+        images.append(pygame.image.load('Resources/Sprites/Mario/walk_r_2.png'))
+        images.append(pygame.image.load('Resources/Sprites/Mario/walk_r_3.png'))
+        images.append(pygame.image.load('Resources/Sprites/Mario/walk_r_4.png'))
+
+    index = 0
+    image = images[index]
+    rect = image.get_rect()
+
+    def update(passed_index, passed_images):
+        passed_index += 1
+        if passed_index >= len(images):
+            passed_index = 0
+        new_image = passed_images[passed_index]
+        return passed_index, new_image
+
+    mario_sprite = pygame.sprite.Sprite()
+    m_group = pygame.sprite.Group(mario_sprite)
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        m_group.update()
+        passed_screen.fill(bgc)
+        m_group.draw(passed_screen)
+        pygame.display.update()
+        clock.tick(10)
+
+
+# run_game()
+
+mario_walk('right', screen, BACKGROUND_COLOR)
